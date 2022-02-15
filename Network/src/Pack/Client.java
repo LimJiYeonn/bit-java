563510ec2db417c9326b74185616b0d53c7cc79a
JiYeon <jiueon6@gmail.com>
2022-02-15 17:55:07 +0900

    Server
    Server

--- Network/src/Pack/Client.java
+++ Network/src/Pack/Client.java
@@ -1,25 +1,88 @@
 package Pack;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Client {
-	public static void main(String[] args) {
-		System.out.println("Client Start");
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Client extends Application{
+	Socket cs; // 모두가 사용하니 field로 정의
+	@Override
+	public void start(Stage stage) throws Exception {
+		VBox root = new VBox();
+		root.setPrefSize(400, 300);
+		root.setSpacing(5);
 		
-		Socket cs = new Socket(); 
-		try {
-			System.out.println("숫자를 입력하면 접속을 시도합니다.");
-			new Scanner(System.in).nextInt();
-			// 접속을 하기위한 정보 넣기
-			cs.connect(new InetSocketAddress("localhost", 5001));
-			
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
+		Button ConnectBtn = new Button("접속 버튼");
+		ConnectBtn.setOnAction(new EventHandler<ActionEvent>() {		
+			@Override
+			public void handle(ActionEvent event) {
+				 cs = new Socket(); 
+				try {
+					cs.connect(new InetSocketAddress("192.168.0.80", 5001));					
+				} catch (Exception e) {e.printStackTrace();}
+			}
+		});
 		
-		System.out.println("숫자를 입력하면 클라이언트를 종료합니다.");
-		new Scanner(System.in).nextInt();
-		System.out.println("Client End");
+		
+		// 서버 접속 후
+		Button btn2 = new Button("데이터전송");
+		btn2.setOnAction(new EventHandler<ActionEvent>() {
+			int count = 0;
+			@Override
+			public void handle(ActionEvent event) {
+				try {
+					OutputStream os = cs.getOutputStream();
+					String s = "apple : " + count++;
+					byte[] data = s.getBytes(); // 생략된 건 보류
+//					System.out.println(data[0]);
+					os.write(data);
+					System.out.println("데이터 보냄");
+				} catch (Exception e) {e.printStackTrace();}
+			}
+		});
+		
+		
+		Button btn3 = new Button("접속 종료");
+		btn3.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				try {cs.close();} catch (Exception e) {} // -1이 서버로 반환
+			}
+		});
+		root.getChildren().addAll(ConnectBtn, btn2, btn3);
+		
+		
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.setTitle("Client");
+		stage.show();
 	}
+	public static void main(String[] args) {
+		launch();
+//		System.out.println("Client Start");
+//		
+//		Socket cs = new Socket(); 
+//		try {
+//			System.out.println("숫자를 입력하면 접속을 시도합니다.");
+//			new Scanner(System.in).nextInt();
+//			// 접속을 하기위한 정보 넣기
+//			cs.connect(new InetSocketAddress("localhost", 5001));
+//			
+//		} catch (Exception e) {
+//			// TODO: handle exception
+//		}
+//		
+//		System.out.println("숫자를 입력하면 클라이언트를 종료합니다.");
+//		new Scanner(System.in).nextInt();
+//		System.out.println("Client End");
+//		
+		}
 }
--- Network/src/Pack/Server.java
+++ Network/src/Pack/Server.java
@@ -1,6 +1,8 @@
 package Pack;
+import java.io.InputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Scanner;
 
 import javafx.application.Application;
@@ -11,6 +13,60 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+//접속 후 데이터 받기
+class ClientThread extends Thread{
+	Socket socket;
+	
+	public ClientThread(Socket socket) {
+		super();
+		this.socket = socket;
+	}
+
+	@Override
+	public void run() {
+		try {
+			InputStream is = socket.getInputStream();
+			byte[] recvData = new byte[512];
+//			int size = is.read(recvData); // 읽으면 recvData에 저장됨
+			// read가 블로킹 함수이다. -> 더이상 진행 안됨
+			// 읽으면 읽은 개수 반환
+			while(true) {
+				int size = is.read(recvData);
+				// 클라이언트가 접속 끊으면 -1을 받음
+				if(size == -1) {
+					System.out.println("클라이언트 접속 종료");
+					break;
+				}
+				String s = new String(recvData, 0, size);
+				System.out.println(s);
+			}
+		} catch (Exception e) {e.printStackTrace();}
+		
+	}
+}
+
+
+class ConnectThread extends Thread{
+	@Override
+	public void run() {
+		try {
+			ServerSocket ss = new ServerSocket();
+			System.out.println("메인서버 소켓 생성");
+			ss.bind(new InetSocketAddress("192.168.0.80", 5001)); //서버의 ip주소
+			// 바인딩 = 자신의 주소를 기억한다. 
+			System.out.println("바인딩완료");
+//			ss.accept(); // 블로킹되어있어 다른 버튼 못 누르는 문제 발생 - 무한루프라 인식 - Thread사용
+			// 접속되면 (서로 연결되어 데이터를 주고 받을 수 있는 )소켓을 받음
+			Socket socket = ss.accept(); // try - catch
+			System.out.println("누군가 접속을 시도했음.");
+			
+			ClientThread ct = new ClientThread(socket);
+			ct.start();
+			
+		} catch (Exception e) {e.printStackTrace(); }
+	}
+}
+
 public class Server extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
@@ -18,29 +74,12 @@ public class Server extends Application {
 		root.setPrefSize(400, 300);
 		root.setSpacing(5);
 		
-		
 		Button openBtn = new Button("서버 오픈");
 		openBtn.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				new Thread() { // 스레드를 사용하는 이유
-					@Override
-					public void run() {
-						try {
-							ServerSocket ss = new ServerSocket();
-							System.out.println("메인서버 소켓 생성");
-							ss.bind(new InetSocketAddress("localhost", 5001));
-							System.out.println("바인딩완료");
-							ss.accept(); // 블로킹되어있어 다른 버튼 못 누르는 문제 발생 - 무한루프라 인식 - Thread사용
-							System.out.println("누군가 접속을 시도했음.");
-						} catch (Exception e) {e.printStackTrace(); }
-					}
-				}.start();
-				
-				
-				
-				
-				
+				ConnectThread cn = new ConnectThread();	
+				cn.run();
 			}
 		});
 		
