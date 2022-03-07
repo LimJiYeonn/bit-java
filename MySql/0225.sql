b0040bac14479639cc67165cb97e2832cadb6e9f
JiYeon <jiueon6@gmail.com>
2022-03-07 19:00:39 +0900

    Update 0225.sql

--- MySql/0225.sql
+++ MySql/0225.sql
@@ -1,9 +1,27 @@
 create database Test;
 create table members (
-index int autoincrement,
+index_id INT NOT NULL AUTO_INCREMENT,
 id varchar(20),
 pwd varchar(20),
 email varchar(20),
 phone varchar(20),
-regdate DATETIME
-)
+regdate DATETIME,
+PRIMARY KEY(index_id)
+);
+
+drop table members;
+create table members (
+id varchar(20),
+pwd varchar(20),
+email varchar(20),
+phone varchar(20),
+regdate DATETIME,
+PRIMARY KEY(id)
+);
+
+insert into members values ('hi', '12', '@', '010', now());
+select * from members;
+
+select * from members;
+
+select pwd from members where id = 'd';
